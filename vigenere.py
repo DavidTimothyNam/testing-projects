17fc1a15af8694a777201dcbea7bd4744d0b2f9c
DavidTimothyNam <87089167+DavidTimothyNam@users.noreply.github.com>
2021-07-09 13:26:55 -0700

    Update vigenere.py
    Removed the input prompts and replaced them with arguments for the function. Also allowed the use of special characters and spaces in the plaintext.

--- vigenere.py
+++ vigenere.py
@@ -1,22 +1,28 @@
-def encode_vigenere():
-    # Grabs the plaintext and the key from the user
-        # Should include a statement that says to remove special characters, and that the output will be in lowercase and have spaces removed
+def encode_vigenere(plaintext, key):
+    plaintext = plaintext.lower()
+    key = key.lower()
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    plaintext = input("What is the text you would like to decrypt? \n").lower().replace(" ", "")
-    key = input("Enter the key you would like to use: ")
-    key_pair = ""
     # By repeating the key, this creates a string of equal length to plain text
+    # By adding to a list of special characters and account for those in the for loop, we can create a key_pair that repeats accurately.
+    key_pair = ""
+    str_all_special_characters = ""
     for x in range(0, len(plaintext)):
-        key_pair += key[x % len(key)]
+        num_special_char = len(str_all_special_char)
+        if plaintext[x] not in alphabet:
+            str_all_special_char += plaintext[x]
+            key_pair += plaintext[x]
+        if plaintext[x] in alphabet:
+            key_pair += key[(x - num_special_char) % len(key)]
     # Takes each letter of the plain text and matches it with the corresponding letter of the key pair.
     # It shifts them by adding their indices together, preforming mod 26, and finding the letter in the alphabet at the position of the sum.
     ciphertext = ""
     for x in range(0, len(plaintext)):
         plain = plaintext[x]
         cipher = key_pair[x]
-        new = alphabet.index(plain) + alphabet.index(cipher)
-        final = alphabet[new % 26]
-        ciphertext += final    
-    print(ciphertext)
-
-encode_vigenere()
+        if cipher not in alphabet:
+            ciphertext += cipher
+        else:
+            new = alphabet.index(plain) + alphabet.index(cipher)
+            final = alphabet[new % 26]
+            ciphertext += final    
+    return(ciphertext)
